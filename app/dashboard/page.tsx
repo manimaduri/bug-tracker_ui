676b14306c306f1a3fa552b38608d57ff76df6df
Maduri Manikanta <manimaduri2@gmail.com>
2024-04-18 08:19:02 +0530

    added mui dependencies and cleared nextjs code
    added route for dashboard and created components folder

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,15 @@
+import { Metadata } from 'next';
+import React from 'react'
+
+export const metadata : Metadata = {
+    title: 'Dashboard',
+    description: 'Dashboard page'
+};  
+
+const DashboardPage = () => {
+  return (
+    <div>DashboardPage</div>
+  )
+}
+
+export default DashboardPage
